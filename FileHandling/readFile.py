52d82c5561aaa2ea9333b6288a46ecf653cc0d3e
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-02 14:00:50 +0300

    check number of python occurrences in the text file

--- FileHandling/readFile.py
+++ FileHandling/readFile.py
@@ -3,4 +3,10 @@ handle = open("test.txt", "r")
 data = handle.read()
 print(data)
 
+# find number of times python occurs in the text file
+counter = 0
+for word in data.split():
+    if word == "Python":
+        counter += 1
+print(f"\n Python occurs {counter} times ")
 handle.close()
