78eae6846afbb372f4b931257695b7184c81ff8b
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-03 10:43:05 +0300

    updated the tests and readfile function

--- FileHandling/Lorem/readFiles.py
+++ FileHandling/Lorem/readFiles.py
@@ -5,7 +5,11 @@ def read_file(txt_file):
     """
     Function that reads a text file and returns the data from the text file.
     :param txt_file:
+    :raises: FileNotFoundError: if it cannot find the file.
     :return: data from the text file
     """
-    with open(txt_file, "r") as handle:
-        return handle.read()
+    try:
+        with open(txt_file, "r") as handle:
+            return handle.read()
+    except FileNotFoundError as e:
+        return None
--- FileHandling/Lorem/test_readfiles.py
+++ FileHandling/Lorem/test_readfiles.py
@@ -7,6 +7,7 @@ class TestReadFiles(unittest.TestCase):
     Class to test the functions on the readFiles modules
     :param unittest.TestCase: Class from the unittest module to create unit tests.
     """
+
     def test_get_data(self):
         """
         Test case to confirm that are getting data from the file.
@@ -15,7 +16,14 @@ class TestReadFiles(unittest.TestCase):
         """
         with open("test.txt", "r") as handle:
             data = handle.read()
-            self.assertEqual(data,readFiles.read_file("test.txt"))
+            self.assertEqual(data, readFiles.read_file("test.txt"))
+
+    def test_nonfile(self):
+        """
+        test to confirm that an exception is raised when a wrong file in inputted
+        :return: boolean True
+        """
+        self.assertEqual(None, readFiles.read_file("tests.txt"))
 
 
 if __name__ == '__main__':
